80f9b72e6c6727935a29a0b0f3a8383c0be3419d
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-10-17 13:17:46 +0200

    set font to Montserrat
    Change-Id: Ia7fe93ba665dadccc91046a1a4022c8cfc2d43ad

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
