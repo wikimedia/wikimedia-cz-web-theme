e1bc7aeecd51bf60e22dd6bbae50f73383921bbd
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-11-29 21:28:08 +0100

    Display the title with background
    Change-Id: I8116050f901c166bcdb44e9a27fd3de553c189f7

--- index.php
+++ index.php
@@ -17,40 +17,42 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-			<h1><?php wp_title('') ?></h1>
-			<div class="wmcz-posts">
-				<?php
-				if ( have_posts() ) :
+			<div class="wmcz-posts-container">
+				<h1><?php wp_title('') ?></h1>
+				<div class="wmcz-posts">
+					<?php
+					if ( have_posts() ) :
 
-					if ( is_home() && ! is_front_page() ) :
-						?>
-						<header>
-							<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-						</header>
-						<?php
+						if ( is_home() && ! is_front_page() ) :
+							?>
+							<header>
+								<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+							</header>
+							<?php
+						endif;
+
+						/* Start the Loop */
+						while ( have_posts() ) :
+							the_post();
+
+							/*
+							* Include the Post-Type-specific template for the content.
+							* If you want to override this in a child theme, then include a file
+							* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+							*/
+							get_template_part( 'template-parts/content-snip', get_post_type() );
+
+						endwhile;
+
+						the_posts_navigation();
+
+					else :
+
+						get_template_part( 'template-parts/content', 'none' );
+
 					endif;
-
-					/* Start the Loop */
-					while ( have_posts() ) :
-						the_post();
-
-						/*
-						* Include the Post-Type-specific template for the content.
-						* If you want to override this in a child theme, then include a file
-						* called content-___.php (where ___ is the Post Type name) and that will be used instead.
-						*/
-						get_template_part( 'template-parts/content-snip', get_post_type() );
-
-					endwhile;
-
-					the_posts_navigation();
-
-				else :
-
-					get_template_part( 'template-parts/content', 'none' );
-
-				endif;
-				?>
+					?>
+				</div>
 			</div>
 
 		</main><!-- #main -->
