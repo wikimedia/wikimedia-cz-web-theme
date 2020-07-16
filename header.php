94365fc2d85b33907d137ac8e3b575a5bad260cd
Martin Urbanec <martin.urbanec@wikimedia.cz>
2020-07-16 12:12:29 +0200

    Make menu look better at mobile
    Change-Id: I0e72a37f097a14d23fb1d09dcf176adbdb064c36

--- header.php
+++ header.php
@@ -36,7 +36,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wmcz' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"></button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
