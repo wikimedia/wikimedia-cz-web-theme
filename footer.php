301af795b3062e6576edea8aab311708855c0ff6
Klára Scholleová <klara@ksch.cz>
2020-07-30 12:37:36 +0200

    add wikiprojects to footer
    Change-Id: Iae2c8e9b01fc0b042db46689bac2b3894199d37c

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,35 +12,54 @@
 
 ?>
 
-	</div><!-- #content -->
+</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="custom-logo-link" rel="home">
-		<img width="1000" height="245" src="https://www.wikimedia.cz/static/images/wmcz_white_logo_245.png" class="custom-logo" alt="Wikimedia ČR">
-	</a>
-
-	<?php
-	wp_nav_menu( array(
-		'theme_location' => 'footer-menu',
-		'menu_id'        => 'footer-menu',
-		'fallback_cb'    => false,
-	) );
-	?>
-
-	<div id="footer-description">
-		<p id="footer-address"><?php echo esc_html( get_theme_mod( 'address' ) ); ?></p>
-		<p id="footer-contact">
-			email: <a href="<?php echo esc_attr( get_theme_mod( 'email' ) ); ?>">
-				<?php echo esc_html( get_theme_mod( 'email' ) ); ?>
-			</a> | telefon: <a href="tel:<?php echo esc_attr( get_theme_mod( 'phone' ) ); ?>">
-				<?php echo esc_html( get_theme_mod( 'phone' ) ); ?>
-			</a>
-		</p>
-	</div>
-	</footer><!-- #colophon -->
 </div><!-- #page -->
+
+<footer id="colophon" class="site-footer">
+	<div class="left">
+		<a href="<?php echo esc_url(home_url('/')); ?>" class="custom-logo-link" rel="home">
+			<img width="1000" height="245" src="https://www.wikimedia.cz/static/images/wmcz_white_logo_245.png" class="custom-logo" alt="Wikimedia ČR">
+		</a>
+
+		<?php
+		wp_nav_menu(array(
+			'theme_location' => 'footer-menu',
+			'menu_id'        => 'footer-menu',
+			'fallback_cb'    => false,
+		));
+		?>
+
+		<div id="footer-description">
+			<p id="footer-address"><?php echo esc_html(get_theme_mod('address')); ?></p>
+			<p id="footer-contact">
+				email: <a href="<?php echo esc_attr(get_theme_mod('email')); ?>">
+					<?php echo esc_html(get_theme_mod('email')); ?>
+				</a> | telefon: <a href="tel:<?php echo esc_attr(get_theme_mod('phone')); ?>">
+					<?php echo esc_html(get_theme_mod('phone')); ?>
+				</a>
+			</p>
+		</div>
+	</div>
+	<div class="right">
+		<a href="https://cs.wikipedia.org/" hreflang="en">Wikipedie</a>
+		<a href="https://commons.wikimedia.org/wiki/Main_Page" hreflang="en">Wikimedia Commons</a>
+		<a href="https://www.mediawiki.org/wiki/MediaWiki" hreflang="en">MediaWiki</a>
+		<a href="https://meta.wikimedia.org/wiki/Main_Page" hreflang="en">Meta-Wiki</a>
+		<a href="https://species.wikimedia.org/wiki/Main_Page" hreflang="en">Wikidruhy</a>
+		<a href="https://cs.wikibooks.org/wiki/Wikiknihy:Hlavn%C3%AD_strana" hreflang="cs">Wikiknihy</a>
+		<a href="https://www.wikidata.org/wiki/Wikidata:Main_Page" hreflang="en">Wikidata</a>
+		<a href="https://wikimania.wikimedia.org/wiki/Wikimania" hreflang="en">Wikimánie</a>
+		<a href="https://cs.wikinews.org/wiki/Wikizpr%C3%A1vy:Hlavn%C3%AD_strana" hreflang="cs">Wikizprávy</a>
+		<a href="https://cs.wikiquote.org/wiki/Wikicit%C3%A1ty:Hlavn%C3%AD_strana" hreflang="cs">Wikicitáty</a>
+		<a href="https://cs.wikisource.org/wiki/Wikizdroje:Hlavn%C3%AD_strana" hreflang="cs">Wikizdroje</a>
+		<a href="https://cs.wikiversity.org/wiki/Wikiverzita:Hlavn%C3%AD_strana" hreflang="cs">Wikiverzita</a>
+		<a href="https://cs.wiktionary.org/wiki/Wikislovn%C3%ADk:Hlavn%C3%AD_strana" hreflang="cs">Wikislovník</a>
+	</div>
+</footer><!-- #colophon -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
