77ae124d43c68f1d9385224dc6e3bf8969e63e9c
Martin Urbanec <martin.urbanec@wikimedia.cz>
2020-11-03 16:32:30 +0100

    Simplify 404.php to avoid having nonsense stuff translated
    Bug: T264708
    Change-Id: Ia2af4f75836890f72f9b96ce1eb0cd2da1ac0309

--- 404.php
+++ 404.php
@@ -19,36 +19,7 @@ get_header();
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'wmcz-theme' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'wmcz-theme' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$wmcz_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'wmcz-theme' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$wmcz_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+					<p><?php esc_html_e( 'It looks like nothing was found at this location.', 'wmcz-theme' ); ?></p>
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
