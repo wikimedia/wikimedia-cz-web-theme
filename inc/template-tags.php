718f9a1a3de4816bf0b75552ea54eb07ff626fef
Martin Urbanec <martin.urbanec@wikimedia.cz>
2020-11-20 18:22:04 +0100

    Do not link thumbnail image
    Change-Id: I9de0dc52f600a42a2aada5513195d93f464dd316

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -122,28 +122,13 @@ if ( ! function_exists( 'wmcz_post_thumbnail' ) ) :
 		if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
 			return;
 		}
+		?>
 
-		if ( is_singular() ) :
-			?>
-
-			<div class="post-thumbnail">
-				<?php the_post_thumbnail(); ?>
-			</div><!-- .post-thumbnail -->
-
-		<?php else : ?>
-
-		<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
-			<?php
-			the_post_thumbnail( 'post-thumbnail', array(
-				'alt' => the_title_attribute( array(
-					'echo' => false,
-				) ),
-			) );
-			?>
-		</a>
+		<div class="post-thumbnail">
+			<?php the_post_thumbnail(); ?>
+		</div><!-- .post-thumbnail -->
 
 		<?php
-		endif; // End is_singular().
 	}
 endif;
 
