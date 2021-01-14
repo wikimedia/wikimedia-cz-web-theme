3e06e7dd2858a42fac50cb3f449319cc7dd091fc
Martin Urbanec <martin.urbanec@wikimedia.cz>
2021-01-14 18:56:24 +0100

    Footer improvements
    Change-Id: I704d3faf641ed0f89c6d77d898ac1e7ce22aa04e

--- footer.php
+++ footer.php
@@ -65,19 +65,21 @@
 		</p>
 	</div>
 	<div class="wikiprojects">
-		<a href="https://cs.wikipedia.org/" hreflang="en"><?php _e('Wikipedia', 'wmcz-theme'); ?></a>
-		<a href="https://commons.wikimedia.org/wiki/Main_Page" hreflang="en"><?php _e('Wikimedia Commons', 'wmcz-theme'); ?></a>
-		<a href="https://www.mediawiki.org/wiki/MediaWiki" hreflang="en"><?php _e('MediaWiki', 'wmcz-theme'); ?></a>
-		<a href="https://meta.wikimedia.org/wiki/Main_Page" hreflang="en"><?php _e('Meta-Wiki', 'wmcz-theme'); ?></a>
-		<a href="https://species.wikimedia.org/wiki/Main_Page" hreflang="en"><?php _e('Wikispecies', 'wmcz-theme'); ?></a>
-		<a href="https://cs.wikibooks.org/wiki/Wikiknihy:Hlavn%C3%AD_strana" hreflang="cs"><?php _e('Wikibooks', 'wmcz-theme'); ?></a>
-		<a href="https://www.wikidata.org/wiki/Wikidata:Main_Page" hreflang="en"><?php _e('Wikidata', 'wmcz-thme'); ?></a>
-		<a href="https://wikimania.wikimedia.org/wiki/Wikimania" hreflang="en"><?php _e('Wikimania', 'wmcz-theme'); ?></a>
-		<a href="https://cs.wikinews.org/wiki/Wikizpr%C3%A1vy:Hlavn%C3%AD_strana" hreflang="cs"><?php _e('Wikinews', 'wmcz-theme'); ?></a>
-		<a href="https://cs.wikiquote.org/wiki/Wikicit%C3%A1ty:Hlavn%C3%AD_strana" hreflang="cs"><?php _e('Wikiquote', 'wmcz-theme'); ?></a>
-		<a href="https://cs.wikisource.org/wiki/Wikizdroje:Hlavn%C3%AD_strana" hreflang="cs"><?php _e('Wikisource', 'wmcz-theme'); ?></a>
-		<a href="https://cs.wikiversity.org/wiki/Wikiverzita:Hlavn%C3%AD_strana" hreflang="cs"><?php _e('Wikiversity', 'wmcz-theme'); ?></a>
-		<a href="https://cs.wiktionary.org/wiki/Wikislovn%C3%ADk:Hlavn%C3%AD_strana" hreflang="cs"><?php _e('Wiktionary', 'wmcz-theme'); ?></a>
+		<ul>
+			<li><a href="https://cs.wikipedia.org/" hreflang="en"><?php _e('Wikipedia', 'wmcz-theme'); ?></a></li>
+			<li><a href="https://commons.wikimedia.org/wiki/Main_Page" hreflang="en"><?php _e('Wikimedia Commons', 'wmcz-theme'); ?></a></li>
+			<li><a href="https://www.mediawiki.org/wiki/MediaWiki" hreflang="en"><?php _e('MediaWiki', 'wmcz-theme'); ?></a></li>
+			<li><a href="https://meta.wikimedia.org/wiki/Main_Page" hreflang="en"><?php _e('Meta-Wiki', 'wmcz-theme'); ?></a></li>
+			<li><a href="https://species.wikimedia.org/wiki/Main_Page" hreflang="en"><?php _e('Wikispecies', 'wmcz-theme'); ?></a></li>
+			<li><a href="https://cs.wikibooks.org/wiki/Wikiknihy:Hlavn%C3%AD_strana" hreflang="cs"><?php _e('Wikibooks', 'wmcz-theme'); ?></a></li>
+			<li><a href="https://www.wikidata.org/wiki/Wikidata:Main_Page" hreflang="en"><?php _e('Wikidata', 'wmcz-thme'); ?></a></li>
+			<li><a href="https://wikimania.wikimedia.org/wiki/Wikimania" hreflang="en"><?php _e('Wikimania', 'wmcz-theme'); ?></a></li>
+			<li><a href="https://cs.wikinews.org/wiki/Wikizpr%C3%A1vy:Hlavn%C3%AD_strana" hreflang="cs"><?php _e('Wikinews', 'wmcz-theme'); ?></a></li>
+			<li><a href="https://cs.wikiquote.org/wiki/Wikicit%C3%A1ty:Hlavn%C3%AD_strana" hreflang="cs"><?php _e('Wikiquote', 'wmcz-theme'); ?></a></li>
+			<li><a href="https://cs.wikisource.org/wiki/Wikizdroje:Hlavn%C3%AD_strana" hreflang="cs"><?php _e('Wikisource', 'wmcz-theme'); ?></a></li>
+			<li><a href="https://cs.wikiversity.org/wiki/Wikiverzita:Hlavn%C3%AD_strana" hreflang="cs"><?php _e('Wikiversity', 'wmcz-theme'); ?></a></li>
+			<li><a href="https://cs.wiktionary.org/wiki/Wikislovn%C3%ADk:Hlavn%C3%AD_strana" hreflang="cs"><?php _e('Wiktionary', 'wmcz-theme'); ?></a></li>
+		</ul>
 	</div>
 </footer><!-- #colophon -->
 
